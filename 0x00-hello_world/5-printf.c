602bda8f7e37b1dab97fa27446887917c05a4f1d
Azi-sambo <youngziggy6@gmail.com>
2023-07-17 18:33:56 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this program that prints exactly
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of");
+	return (0);
+}
